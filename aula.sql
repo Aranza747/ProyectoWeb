67d7a580e495dc4c7c24d2c9c7501833fafb1ab1
micheellpueblita <aracelimichel@comunidad.unam.mx>
2022-05-31 14:29:34 -0500

    refactor(docs): nueva base de datos

--- aula.sql
+++ aula.sql
@@ -23,25 +23,19 @@ DROP TABLE IF EXISTS `alumno`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `alumno` (
-  `Alumno_ID` int(11) NOT NULL AUTO_INCREMENT,
-  `Foro_ID` int(11) NOT NULL,
-  `Tablon_ID` int(11) NOT NULL,
-  `No_Cuenta` int(11) NOT NULL,
-  `Nombre` char(100) NOT NULL,
-  `Usuario` varchar(50) NOT NULL,
-  `Correo` varchar(50) NOT NULL,
-  `Grupo_ID` int(11) NOT NULL,
-  `Contraseña` varchar(20) NOT NULL,
-  PRIMARY KEY (`Alumno_ID`),
-  UNIQUE KEY `No_Cuenta` (`No_Cuenta`),
-  UNIQUE KEY `Usuario` (`Usuario`),
-  UNIQUE KEY `Correo` (`Correo`),
-  KEY `Foro_ID` (`Foro_ID`),
-  KEY `Tablon_ID` (`Tablon_ID`),
-  KEY `Grupo_ID` (`Grupo_ID`),
-  CONSTRAINT `alumno_ibfk_1` FOREIGN KEY (`Foro_ID`) REFERENCES `foro` (`Foro_ID`),
-  CONSTRAINT `alumno_ibfk_2` FOREIGN KEY (`Tablon_ID`) REFERENCES `tablon` (`Tablon_ID`),
-  CONSTRAINT `alumno_ibfk_3` FOREIGN KEY (`Grupo_ID`) REFERENCES `grupo` (`Grupo_ID`)
+  `id_alumno` int(11) NOT NULL AUTO_INCREMENT,
+  `noDeCuenta` int(11) NOT NULL,
+  `nombre` char(100) NOT NULL,
+  `usuario` varchar(50) NOT NULL,
+  `correo` varchar(50) NOT NULL,
+  `id_materia` int(11) DEFAULT NULL,
+  `contraseña` varchar(20) NOT NULL,
+  PRIMARY KEY (`id_alumno`),
+  UNIQUE KEY `noDeCuenta` (`noDeCuenta`),
+  UNIQUE KEY `usuario` (`usuario`),
+  UNIQUE KEY `correo` (`correo`),
+  KEY `id_materia` (`id_materia`),
+  CONSTRAINT `alumno_ibfk_1` FOREIGN KEY (`id_materia`) REFERENCES `materia` (`id_materia`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -55,74 +49,154 @@ LOCK TABLES `alumno` WRITE;
 UNLOCK TABLES;
 
 --
--- Table structure for table `archivo`
+-- Table structure for table `archivotablon`
 --
 
-DROP TABLE IF EXISTS `archivo`;
+DROP TABLE IF EXISTS `archivotablon`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `archivo` (
-  `Archivo_ID` int(11) NOT NULL AUTO_INCREMENT,
-  `Ruta` varchar(100) NOT NULL,
-  `Fecha_entrega` datetime NOT NULL,
-  PRIMARY KEY (`Archivo_ID`),
-  UNIQUE KEY `Ruta` (`Ruta`)
+CREATE TABLE `archivotablon` (
+  `id_archivoTablon` int(11) NOT NULL AUTO_INCREMENT,
+  `ruta` varchar(100) NOT NULL,
+  `fechaEntrega` date NOT NULL,
+  PRIMARY KEY (`id_archivoTablon`),
+  UNIQUE KEY `ruta` (`ruta`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `archivo`
+-- Dumping data for table `archivotablon`
 --
 
-LOCK TABLES `archivo` WRITE;
-/*!40000 ALTER TABLE `archivo` DISABLE KEYS */;
-/*!40000 ALTER TABLE `archivo` ENABLE KEYS */;
+LOCK TABLES `archivotablon` WRITE;
+/*!40000 ALTER TABLE `archivotablon` DISABLE KEYS */;
+/*!40000 ALTER TABLE `archivotablon` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `calendario`
+-- Table structure for table `archivotarea`
 --
 
-DROP TABLE IF EXISTS `calendario`;
+DROP TABLE IF EXISTS `archivotarea`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `calendario` (
-  `Calendario_ID` int(11) NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (`Calendario_ID`)
+CREATE TABLE `archivotarea` (
+  `id_archivoTarea` int(11) NOT NULL AUTO_INCREMENT,
+  `ruta` varchar(100) NOT NULL,
+  `fechaEntrega` date NOT NULL,
+  PRIMARY KEY (`id_archivoTarea`),
+  UNIQUE KEY `ruta` (`ruta`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `calendario`
+-- Dumping data for table `archivotarea`
 --
 
-LOCK TABLES `calendario` WRITE;
-/*!40000 ALTER TABLE `calendario` DISABLE KEYS */;
-/*!40000 ALTER TABLE `calendario` ENABLE KEYS */;
+LOCK TABLES `archivotarea` WRITE;
+/*!40000 ALTER TABLE `archivotarea` DISABLE KEYS */;
+/*!40000 ALTER TABLE `archivotarea` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `comentario`
+-- Table structure for table `calendarioglobal`
 --
 
-DROP TABLE IF EXISTS `comentario`;
+DROP TABLE IF EXISTS `calendarioglobal`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `comentario` (
-  `Comentario_ID` int(11) NOT NULL AUTO_INCREMENT,
-  `Comentario` char(200) NOT NULL,
-  `Fecha_del_comentario` datetime NOT NULL,
-  PRIMARY KEY (`Comentario_ID`)
+CREATE TABLE `calendarioglobal` (
+  `id_calendarioGlobal` int(11) NOT NULL AUTO_INCREMENT,
+  `evento` text DEFAULT NULL,
+  `descripcion` text DEFAULT NULL,
+  `fecha` date NOT NULL,
+  `hora` time NOT NULL,
+  PRIMARY KEY (`id_calendarioGlobal`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `comentario`
+-- Dumping data for table `calendarioglobal`
 --
 
-LOCK TABLES `comentario` WRITE;
-/*!40000 ALTER TABLE `comentario` DISABLE KEYS */;
-/*!40000 ALTER TABLE `comentario` ENABLE KEYS */;
+LOCK TABLES `calendarioglobal` WRITE;
+/*!40000 ALTER TABLE `calendarioglobal` DISABLE KEYS */;
+/*!40000 ALTER TABLE `calendarioglobal` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `calendariopersonal`
+--
+
+DROP TABLE IF EXISTS `calendariopersonal`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `calendariopersonal` (
+  `id_calendarioPersonal` int(11) NOT NULL AUTO_INCREMENT,
+  `id_calendarioGlobal` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id_calendarioPersonal`),
+  KEY `id_calendarioGlobal` (`id_calendarioGlobal`),
+  CONSTRAINT `calendariopersonal_ibfk_1` FOREIGN KEY (`id_calendarioGlobal`) REFERENCES `calendarioglobal` (`id_calendarioGlobal`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `calendariopersonal`
+--
+
+LOCK TABLES `calendariopersonal` WRITE;
+/*!40000 ALTER TABLE `calendariopersonal` DISABLE KEYS */;
+/*!40000 ALTER TABLE `calendariopersonal` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `comentarioforo`
+--
+
+DROP TABLE IF EXISTS `comentarioforo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `comentarioforo` (
+  `id_comentarioForo` int(11) NOT NULL AUTO_INCREMENT,
+  `comentario` text NOT NULL,
+  `fechaDeComentario` date NOT NULL,
+  `hora` time NOT NULL,
+  PRIMARY KEY (`id_comentarioForo`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `comentarioforo`
+--
+
+LOCK TABLES `comentarioforo` WRITE;
+/*!40000 ALTER TABLE `comentarioforo` DISABLE KEYS */;
+/*!40000 ALTER TABLE `comentarioforo` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `comentariotarea`
+--
+
+DROP TABLE IF EXISTS `comentariotarea`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `comentariotarea` (
+  `id_comentarioTarea` int(11) NOT NULL AUTO_INCREMENT,
+  `comentario` text NOT NULL,
+  `fechaDeComentario` date NOT NULL,
+  `hora` time NOT NULL,
+  PRIMARY KEY (`id_comentarioTarea`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `comentariotarea`
+--
+
+LOCK TABLES `comentariotarea` WRITE;
+/*!40000 ALTER TABLE `comentariotarea` DISABLE KEYS */;
+/*!40000 ALTER TABLE `comentariotarea` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -133,11 +207,17 @@ DROP TABLE IF EXISTS `foro`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `foro` (
-  `Foro_ID` int(11) NOT NULL AUTO_INCREMENT,
-  `Comentario_ID` int(11) NOT NULL,
-  PRIMARY KEY (`Foro_ID`),
-  KEY `Comentario_ID` (`Comentario_ID`),
-  CONSTRAINT `foro_ibfk_1` FOREIGN KEY (`Comentario_ID`) REFERENCES `comentario` (`Comentario_ID`)
+  `id_foro` int(11) NOT NULL AUTO_INCREMENT,
+  `id_comentarioForo` int(11) DEFAULT NULL,
+  `id_alumno` int(11) DEFAULT NULL,
+  `id_rol` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id_foro`),
+  KEY `id_comentarioForo` (`id_comentarioForo`),
+  KEY `id_alumno` (`id_alumno`),
+  KEY `id_rol` (`id_rol`),
+  CONSTRAINT `foro_ibfk_1` FOREIGN KEY (`id_comentarioForo`) REFERENCES `comentarioforo` (`id_comentarioForo`),
+  CONSTRAINT `foro_ibfk_2` FOREIGN KEY (`id_alumno`) REFERENCES `alumno` (`id_alumno`),
+  CONSTRAINT `foro_ibfk_3` FOREIGN KEY (`id_rol`) REFERENCES `roles` (`id_rol`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -151,26 +231,62 @@ LOCK TABLES `foro` WRITE;
 UNLOCK TABLES;
 
 --
--- Table structure for table `grupo`
+-- Table structure for table `juegomemorama`
 --
 
-DROP TABLE IF EXISTS `grupo`;
+DROP TABLE IF EXISTS `juegomemorama`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `grupo` (
-  `Grupo_ID` int(11) NOT NULL AUTO_INCREMENT,
-  `Grupo` tinyint(5) NOT NULL,
-  PRIMARY KEY (`Grupo_ID`)
+CREATE TABLE `juegomemorama` (
+  `id_juegoMemorama` int(11) NOT NULL AUTO_INCREMENT,
+  `id_materia` int(11) DEFAULT NULL,
+  `calificacionMemorama` decimal(5,0) NOT NULL,
+  `pregunta` char(100) NOT NULL,
+  `respuesta` char(200) NOT NULL,
+  PRIMARY KEY (`id_juegoMemorama`),
+  KEY `id_materia` (`id_materia`),
+  CONSTRAINT `juegomemorama_ibfk_1` FOREIGN KEY (`id_materia`) REFERENCES `materia` (`id_materia`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `grupo`
+-- Dumping data for table `juegomemorama`
 --
 
-LOCK TABLES `grupo` WRITE;
-/*!40000 ALTER TABLE `grupo` DISABLE KEYS */;
-/*!40000 ALTER TABLE `grupo` ENABLE KEYS */;
+LOCK TABLES `juegomemorama` WRITE;
+/*!40000 ALTER TABLE `juegomemorama` DISABLE KEYS */;
+/*!40000 ALTER TABLE `juegomemorama` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `juegotrivia`
+--
+
+DROP TABLE IF EXISTS `juegotrivia`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `juegotrivia` (
+  `id_juegoTrivia` int(11) NOT NULL AUTO_INCREMENT,
+  `id_materia` int(11) DEFAULT NULL,
+  `calificacionTrivia` decimal(5,0) NOT NULL,
+  `pregunta` char(100) NOT NULL,
+  `opcion1` char(100) NOT NULL,
+  `opcion2` char(100) NOT NULL,
+  `opcion3` char(100) NOT NULL,
+  `respuesta` char(100) NOT NULL,
+  PRIMARY KEY (`id_juegoTrivia`),
+  KEY `id_materia` (`id_materia`),
+  CONSTRAINT `juegotrivia_ibfk_1` FOREIGN KEY (`id_materia`) REFERENCES `materia` (`id_materia`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `juegotrivia`
+--
+
+LOCK TABLES `juegotrivia` WRITE;
+/*!40000 ALTER TABLE `juegotrivia` DISABLE KEYS */;
+/*!40000 ALTER TABLE `juegotrivia` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -181,15 +297,13 @@ DROP TABLE IF EXISTS `materia`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `materia` (
-  `Materia_ID` int(11) NOT NULL AUTO_INCREMENT,
-  `Link_Reunion` varchar(50) DEFAULT NULL,
-  `Descripcion` varchar(200) NOT NULL,
-  `Grupo_ID` int(11) NOT NULL,
-  `Foto` varchar(100) NOT NULL,
-  `Calificacion_final` tinyint(5) NOT NULL,
-  PRIMARY KEY (`Materia_ID`),
-  KEY `Grupo_ID` (`Grupo_ID`),
-  CONSTRAINT `materia_ibfk_1` FOREIGN KEY (`Grupo_ID`) REFERENCES `grupo` (`Grupo_ID`)
+  `id_materia` int(11) NOT NULL AUTO_INCREMENT,
+  `linkReunion` varchar(50) DEFAULT NULL,
+  `descripcion` varchar(200) NOT NULL,
+  `contrasena` varchar(100) NOT NULL,
+  `foto` varchar(100) NOT NULL,
+  `calificacionFinal` tinyint(5) NOT NULL,
+  PRIMARY KEY (`id_materia`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -210,12 +324,12 @@ DROP TABLE IF EXISTS `modulo`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `modulo` (
-  `Modulo_ID` int(11) NOT NULL AUTO_INCREMENT,
-  `Materia_ID` int(11) NOT NULL,
-  `Calificacion` tinyint(5) NOT NULL,
-  PRIMARY KEY (`Modulo_ID`),
-  KEY `Materia_ID` (`Materia_ID`),
-  CONSTRAINT `modulo_ibfk_1` FOREIGN KEY (`Materia_ID`) REFERENCES `materia` (`Materia_ID`)
+  `id_modulo` int(11) NOT NULL AUTO_INCREMENT,
+  `id_materia` int(11) DEFAULT NULL,
+  `calificacion` decimal(5,0) NOT NULL,
+  PRIMARY KEY (`id_modulo`),
+  KEY `id_materia` (`id_materia`),
+  CONSTRAINT `modulo_ibfk_1` FOREIGN KEY (`id_materia`) REFERENCES `materia` (`id_materia`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -229,6 +343,37 @@ LOCK TABLES `modulo` WRITE;
 UNLOCK TABLES;
 
 --
+-- Table structure for table `roles`
+--
+
+DROP TABLE IF EXISTS `roles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `roles` (
+  `id_rol` int(11) NOT NULL AUTO_INCREMENT,
+  `noDeCuenta` int(11) NOT NULL,
+  `correo` varchar(50) NOT NULL,
+  `contrasena` varchar(20) NOT NULL,
+  `rol` varchar(15) NOT NULL,
+  `id_materia` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id_rol`),
+  UNIQUE KEY `noDeCuenta` (`noDeCuenta`),
+  UNIQUE KEY `correo` (`correo`),
+  KEY `id_materia` (`id_materia`),
+  CONSTRAINT `roles_ibfk_1` FOREIGN KEY (`id_materia`) REFERENCES `materia` (`id_materia`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `roles`
+--
+
+LOCK TABLES `roles` WRITE;
+/*!40000 ALTER TABLE `roles` DISABLE KEYS */;
+/*!40000 ALTER TABLE `roles` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `tablon`
 --
 
@@ -236,11 +381,17 @@ DROP TABLE IF EXISTS `tablon`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `tablon` (
-  `Tablon_ID` int(11) NOT NULL AUTO_INCREMENT,
-  `Archivo_ID` int(11) NOT NULL,
-  PRIMARY KEY (`Tablon_ID`),
-  KEY `Archivo_ID` (`Archivo_ID`),
-  CONSTRAINT `tablon_ibfk_1` FOREIGN KEY (`Archivo_ID`) REFERENCES `archivo` (`Archivo_ID`)
+  `id_tablon` int(11) NOT NULL AUTO_INCREMENT,
+  `id_archivoTablon` int(11) DEFAULT NULL,
+  `id_alumno` int(11) DEFAULT NULL,
+  `id_rol` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id_tablon`),
+  KEY `id_alumno` (`id_alumno`),
+  KEY `id_archivoTablon` (`id_archivoTablon`),
+  KEY `id_rol` (`id_rol`),
+  CONSTRAINT `tablon_ibfk_1` FOREIGN KEY (`id_alumno`) REFERENCES `alumno` (`id_alumno`),
+  CONSTRAINT `tablon_ibfk_2` FOREIGN KEY (`id_archivoTablon`) REFERENCES `archivotablon` (`id_archivoTablon`),
+  CONSTRAINT `tablon_ibfk_3` FOREIGN KEY (`id_rol`) REFERENCES `roles` (`id_rol`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -261,22 +412,23 @@ DROP TABLE IF EXISTS `tarea`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `tarea` (
-  `Tarea_ID` int(11) NOT NULL AUTO_INCREMENT,
-  `Modulo_ID` int(11) NOT NULL,
-  `Calendario_ID` int(11) NOT NULL,
-  `Archivo_ID` int(11) NOT NULL,
-  `Comentario_ID` int(11) NOT NULL,
-  `Fecha` datetime NOT NULL,
-  `Calificacion` tinyint(5) NOT NULL,
-  PRIMARY KEY (`Tarea_ID`),
-  KEY `Modulo_ID` (`Modulo_ID`),
-  KEY `Calendario_ID` (`Calendario_ID`),
-  KEY `Archivo_ID` (`Archivo_ID`),
-  KEY `Comentario_ID` (`Comentario_ID`),
-  CONSTRAINT `tarea_ibfk_1` FOREIGN KEY (`Modulo_ID`) REFERENCES `modulo` (`Modulo_ID`),
-  CONSTRAINT `tarea_ibfk_2` FOREIGN KEY (`Calendario_ID`) REFERENCES `calendario` (`Calendario_ID`),
-  CONSTRAINT `tarea_ibfk_3` FOREIGN KEY (`Archivo_ID`) REFERENCES `archivo` (`Archivo_ID`),
-  CONSTRAINT `tarea_ibfk_4` FOREIGN KEY (`Comentario_ID`) REFERENCES `comentario` (`Comentario_ID`)
+  `id_tarea` int(11) NOT NULL AUTO_INCREMENT,
+  `id_modulo` int(11) DEFAULT NULL,
+  `id_calendarioPersonal` int(11) DEFAULT NULL,
+  `id_archivoTarea` int(11) DEFAULT NULL,
+  `id_comentarioTarea` int(11) DEFAULT NULL,
+  `fecha` date NOT NULL,
+  `hora` time NOT NULL,
+  `calificacion` decimal(5,0) NOT NULL,
+  PRIMARY KEY (`id_tarea`),
+  KEY `id_modulo` (`id_modulo`),
+  KEY `id_calendarioPersonal` (`id_calendarioPersonal`),
+  KEY `id_archivoTarea` (`id_archivoTarea`),
+  KEY `id_comentarioTarea` (`id_comentarioTarea`),
+  CONSTRAINT `tarea_ibfk_1` FOREIGN KEY (`id_modulo`) REFERENCES `modulo` (`id_modulo`),
+  CONSTRAINT `tarea_ibfk_2` FOREIGN KEY (`id_calendarioPersonal`) REFERENCES `calendariopersonal` (`id_calendarioPersonal`),
+  CONSTRAINT `tarea_ibfk_3` FOREIGN KEY (`id_archivoTarea`) REFERENCES `archivotarea` (`id_archivoTarea`),
+  CONSTRAINT `tarea_ibfk_4` FOREIGN KEY (`id_comentarioTarea`) REFERENCES `comentariotarea` (`id_comentarioTarea`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -298,4 +450,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-05-18 18:31:41
+-- Dump completed on 2022-05-31 14:13:22
