5a92ba8c32d92b5b8d075a835445d35e2ff67cc4
micheellpueblita <aracelimichel@comunidad.unam.mx>
2022-06-01 22:03:13 -0500

    Fix: Validacion del formulario, falta consultar que no exista el usuario

--- dynamics/php/registro.php
+++ dynamics/php/registro.php
@@ -3,8 +3,7 @@ require "config.php";
 
 $con = mysqli_connect($db_host, $db_user, $db_pass, $db_schema);
 
-$noCuenta = (isset($_POST["numeroCuenta"]) && $_POST["numeroCuenta"]!= "")?$_POST["numeroCuenta"]:false;
-
+$noCuentaRFC = (isset($_POST["numeroCuenta"]) && $_POST["numeroCuenta"]!= "")?$_POST["numeroCuenta"]:false;
 $nombre = (isset($_POST["nombre"]) && $_POST["nombre"]!= "")?$_POST["nombre"]:false;//Condicionar Si es estudiante o profesor
 $correo = (isset($_POST["correo"]) && $_POST["correo"]!= "")?$_POST["correo"]:false;
 $usuario = (isset($_POST["usuario"]) && $_POST["usuario"]!= "")?$_POST["usuario"]:false;
