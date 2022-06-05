a19ead0cb6a6f18d5fa51f65b6fbd0563b5775de
Mari <321244763@alumno.enp.unam.mx>
2022-06-05 13:14:29 -0500

    style: ya todos tienen el aside :)

--- dynamics/php/formCrearMateria.php
+++ dynamics/php/formCrearMateria.php
@@ -23,27 +23,36 @@
     <!--  Navbar -->
     <nav class="navbar navbar-expand-lg" id="nav">
         <div class="container-fluid">
-
-        <a class="navbar-brand" href="https://www.prepa6.unam.mx/ENP6/_P6/">
-                <img src="../../statics/img/logoprepa.png" alt="" width="40" height="40">
+            <a class="navbar-brand" href="https://www.prepa6.unam.mx/ENP6/_P6/">
+                <img src="../../statics/img/logoprepa.png" alt="" width="50" height="50">
             </a>
             <!-- Necesario solo cuando no sea la vista principal -->
-
-
             <div class="navbar-nav">
                 <div>
                 <a class="navbar-brand" href="./pagInicio.php">
-                        <img id="inicio" src="../../statics/img/Logoaula.png" alt="" width="30" height="30">
+                        <img id="inicio" src="../../statics/img/Logoaula.png" alt="" width="40" height="40">
                     </a>
                 </div>
                 <div>
-                    <a class="nav-link" href="#" id="perfil">Perfil</a>
+                    <button class="list-group-item btn-opcion" id="foro" class="btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">Perfil</button>
                 </div>
             </div>
         </div>
     </nav>
 
     <aside>
+        <!-- columna-->
+        <ul class="list-group list-group-flush"> 
+            
+            
+            <button type="button" class="list-group-item btn-opcion" id="foro" onclick="location.href='./vistaForoPreguntasFrec.php'">Foro </button>
+            <button type="button" class="list-group-item btn-opcion" id="tablon">Tablon</button>
+            <button type="button" class="list-group-item btn-opcion" id="calendario">Calendario</button>
+            <button type="button" class="list-group-item btn-opcion" id="crear" onclick="location.href='./formCrearMateria.php'">Crear Materia</button>  <!-- Profesor-->
+            <button type="button" class="list-group-item btn-opcion" id="crear">Calificaciones</button> <!-- Alumno -->
+            <button type="button" class="list-group-item btn-opcion" id="crear">Participantes</button> <!-- Administrador-->
+            
+        </ul>
     </aside>
 
     <!-- crear materia -->
@@ -85,7 +94,7 @@
                         <br/><br/>
                     </div>
                     <div>
-                        <button type="submit" id="crear">Crear</button>
+                        <button type="submit" id="crear" class="crear">Crear</button>
                     </div>
                 </div>
             </div>
