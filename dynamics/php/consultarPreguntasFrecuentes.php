f9b5033e4a0dfe5f98e5c6f6afe66e6b2fb45689
Aranza747 <aranzaromero@comunidad.unam.mx>
2022-06-05 02:35:58 -0500

    feat(foro y materia): arreglando rutas

---
+++ dynamics/php/consultarPreguntasFrecuentes.php
@@ -0,0 +1,11 @@
+<?php
+
+require "config.php";
+
+$con = mysqli_connect($db_host, $db_user, $db_pass, $db_schema);
+
+if(!$con){
+    echo "No se pudo conectar";
+}else{
+    $sql = "SELECT preguntasFrecuentes "
+}
