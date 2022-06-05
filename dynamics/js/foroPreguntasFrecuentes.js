0376ea8a11f5926f42a9acb78c71e7d751fc7c80
Aranza747 <aranzaromero@comunidad.unam.mx>
2022-06-05 01:09:52 -0500

    feat(foro): mostrando preguntas frecuentes

--- dynamics/js/foroPreguntasFrecuentes.js
+++ dynamics/js/foroPreguntasFrecuentes.js
@@ -1,8 +1,13 @@
 const divPreguntasFrecuentes = document.getElementById("preguntasFrecuentes")
-
-var html = "<div id='preguntasFrecuentes' class='card border-primary mb-3' style='max-width: 18rem;'><div class='card-header'>Header</div><div class='card-body text-primary'><h5 class='card-title'>Primary card title</h5><p class='card-text'>Some quick example text to build on the card title and make up the bulk of the card's content.</p></div></div></div>";
-
 const añadir = document.getElementById("añadir")
+const guardar = document.getElementById("guardarCambios")
+
+var html = "<div id='preguntasFrecuentes' class='card border-primary mb-3 preguntasFrecuentes' style='max-width: 1000px;'></div>"+
+            "<div class='card-header'>Header</div>"+
+            "<div class='card-body text-primary'><h5 class='card-title'>Primary card title</h5>"+
+            "<p class='card-text'>Some quick example text to build on the card title and make up the bulk of the card's content.</p>"+
+            "</div></div>";
+
 añadir.addEventListener("click", (evento) => {
     divPreguntasFrecuentes.innerHTML += html;
 })
