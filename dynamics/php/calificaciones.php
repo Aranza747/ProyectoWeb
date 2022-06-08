b59d2bad111eac145aed7881d006833f77926cad
Aranza747 <aranzaromero@comunidad.unam.mx>
2022-06-07 20:56:17 -0500

    feat(php):maquetado calificaciones

---
+++ dynamics/php/calificaciones.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calificaciones</title>
+    <link rel="stylesheet" href="../../libs/bootstrap-5.2.0-beta1-dist/css/bootstrap.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="../../statics/styles/calificaciones.css">
+</head>
+<body>
+        <!--  Navbar -->
+        <nav class="navbar navbar-expand-lg" id="nav">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="https://www.prepa6.unam.mx/ENP6/_P6/">
+                <img src="../../statics/img/logoprepa.png" alt="" width="50" height="50">
+            </a>
+            <!-- Necesario solo cuando no sea la vista principal -->
+            <div class="navbar-nav">
+                <div>
+                    <form action='./CerrarSesion.php' method='post' turget='_self'>
+                        <button id="cerrar">Cerrar Sesión</button>
+                    </form>
+                </div>
+                <div>
+                    <button class="list-group-item btn-opcion" id="foro" class="btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">Perfil</button>
+                </div>
+            </div>
+        </div>
+    </nav>
+    
+    <?php    
+        echo '<div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel" width="15%" height="15%">
+            <div class="offcanvas-header">
+                <span class="offcanvas-title" id="offcanvasRightLabel">Perfil</span>
+                <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            </div>
+            <div class="offcanvas-body" id="padre">
+                    <img  id="hijo" src="../../statics/img/descargasfondoinicio.jpeg" alt="perfil" width="80%" height="25%">
+                <div class="hijo">
+                    <dl><dt class="cabeza">Nombre: &nbsp</dt>'.'<dd class="dato"> &nbsp &nbsp &nbsp &nbsp'.$_SESSION["nombre"].'
+                        </dd>
+                    </dl>
+
+                    <dl><dt class="cabeza">Correo: &nbsp</dt>'.'<dd class="dato"> &nbsp &nbsp &nbsp &nbsp'.$_SESSION["correo"].'
+                        </dd>
+                    </dl>
+
+                    <dl><dt class="cabeza">Rol: &nbsp</dt>'.'<dd class="dato"> &nbsp &nbsp &nbsp &nbsp'.$_SESSION["rol"].'
+                    </dd>
+                    </dl> 
+                </div>
+            </div>
+        </div>'
+    ?>
+    <!-- Perfil -->
+    
+
+    <aside>
+        <!-- columna-->
+        <ul class="list-group list-group-flush"> 
+            
+            <button type="button" class="list-group-item btn-opcion" id="foro" onclick="location.href='./vistaForoPreguntasFrec.php'">Foro </button>
+            <button type="button" class="list-group-item btn-opcion" id="tablon">Tablon</button>
+            <button type="button" class="list-group-item btn-opcion" id="calendario" onclick="location.href='./calendario.php'">Calendario</button>
+            <?php
+                if($_SESSION["rol"] == "Profesor"){
+                    echo '<button type="button" class="list-group-item btn-opcion" id="crear" onclick="location.href=\'./formCrearMateria.php\'">Crear Materia</button>'  ;
+                } else if ($_SESSION["rol"] == "Administrador"){
+                    echo '<button type="button" class="list-group-item btn-opcion" id="crear" onclick="location.href=\'./formCrearMateria.php\'">Crear Materia</button>'  ;
+                    echo '<button type="button" class="list-group-item btn-opcion" id="crear">Participantes</button>'; 
+                } else if ($_SESSION["rol"] == "Alumno"){
+                    echo '<button type="button" class="list-group-item btn-opcion" id="crear">Calificaciones</button>'; 
+                } else if ($_SESSION["rol"] == "Moderador"){
+
+                }
+            ?>
+            
+        </ul>
+    </aside>   
+    <?php
+        if($_SESSION["rol"] == "Alumno"){
+            // echo '<button type="button" class="list-group-item btn-opcion" id="crear" onclick="location.href="./formCrearMateria.php"">Crear Materia</button>'  
+        echo '
+            <div class="d-grid gap-2 col-6 mx-auto">
+                <button class="btn btn-primary" type="button" onclick="location.href=\'./formIngresarMateria.php\'">Ingresar a curso nuevo</button>
+            </div>';
+        }
+    ?>
+
+    
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="../jsredireccionar.js"></script>
+</body>
+</html>
